632d1f01e09ec314022a04efb1d84999b0fafa30
nobrega <gabrielnobregasantos2@gmail.com>
2025-07-30 22:00:36 -0300

    fix: update imports

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -19,7 +19,7 @@ import SignUp from "./pages/auth/SignUp";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { AuthProvider } from "./components/auth/AuthContext";
 import FirstSteps from "./pages/auth/primeiros-passos/FirstSteps";
-import NewCategorie from "./pages/principal/categoria/NewCategorie";
+import NewCategorie from "./pages/principal/categoria/NewCategoria";
 
 function App() {
   return (
